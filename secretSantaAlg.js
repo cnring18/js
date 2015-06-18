f353f91fef4977986b130452d80eb45eb6ff8436
cnring18 <charlesring@Charles-Rings-MacBook-Pro.local>
2015-06-18 14:46:15 -0400

    test function

---
+++ secretSantaAlg.js
@@ -0,0 +1,24 @@
+var a = ['aaaaa','zzzzz','rrrrr','fffff','ooooo','wwwww','ddddd','ccccc','jjjjj'];
+ 
+function r(array) {
+    var a,b, p1, p2, i, n, rand,x, final = [];
+    n = array.length;
+    i = 0;
+    a = array.slice(0);
+    b = array.slice(0);
+    while(i<n) {
+        p1 = a[0];
+        x = b.indexOf(p1);
+        do {
+            rand = Math.floor((Math.random() * b.length));
+        } while(rand === x)
+        p2 = b[rand];
+        final[p1] = p2;
+        a.splice(0,1);
+        b.splice(rand,1);       
+        i++;
+    }  
+    console.log(final);
+}
+ 
+r(a);
