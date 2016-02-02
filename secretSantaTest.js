93359a521a8c6e2ba0eb30aa4f8a2c6f1561b226
cnring18 <cnring18@gmail.com>
2016-02-02 12:53:53 -0500

    Added comments to test function for secret santa app

--- secretSantaTest.js
+++ secretSantaTest.js
@@ -10,12 +10,12 @@
 var people = ['aa','zz','rr','ff','oo','ww','dd','cc','jj'];
  
 function r(array) {
-    var a = array.slice(0),
+    var a = array.slice(0),                 //copying inital array into giving/receiving arrays
         b = array.slice(0),
-        p1, p2,
+        p1, p2,                             //giving/receiving person
         i = 0,
         n = array.length, 
-        rand,x, finalPairs = [];
+        rand, x, finalPairs = [];           //random index to receive, index of giving person in receiving array, final give/receive pairings
     while(i<n) {
         p1 = a[0];
         x = b.indexOf(p1);
@@ -24,8 +24,8 @@ function r(array) {
         } while(rand === x)
         p2 = b[rand];
         finalPairs[p1] = p2;
-        a.splice(0,1);
-        b.splice(rand,1);       
+        a.splice(0,1);                      //remove first person from giving list (used)
+        b.splice(rand,1);                   //remove random (used) person from receiving list
         i++;
     }  
     return finalPairs;
