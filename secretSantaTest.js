2ee118f0a585e5bd60a7c5e09310600c6b960542
cnring18 <cnring18@gmail.com>
2016-02-02 12:48:32 -0500

    Testing function for secret santa app

--- secretSantaTest.js
+++ secretSantaTest.js
@@ -1,13 +1,21 @@
+/*
+    Testing function for a secret santa app
+    All people (in array) get paired with another person that is NOT themselves
+    Function output is array of each person and who they give to
+*/
+
+
+
 //secret santa function
-var a = ['aaaaa','zzzzz','rrrrr','fffff','ooooo','wwwww','ddddd','ccccc','jjjjj'];
+var people = ['aa','zz','rr','ff','oo','ww','dd','cc','jj'];
  
 function r(array) {
     var a = array.slice(0),
-        b = array.slice(0)
+        b = array.slice(0),
         p1, p2,
         i = 0,
         n = array.length, 
-        rand,x, final = [];
+        rand,x, finalPairs = [];
     while(i<n) {
         p1 = a[0];
         x = b.indexOf(p1);
@@ -15,12 +23,12 @@ function r(array) {
             rand = Math.floor((Math.random() * b.length));
         } while(rand === x)
         p2 = b[rand];
-        final[p1] = p2;
+        finalPairs[p1] = p2;
         a.splice(0,1);
         b.splice(rand,1);       
         i++;
     }  
-    console.log(final);
+    return finalPairs;
 }
  
-r(a);
+console.log(r(people));
